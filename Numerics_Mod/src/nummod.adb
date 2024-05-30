a1e2864769d0643192d06aa5914faf96b75c4c81
0dayx16 <91608871+ayaanlehashi11@users.noreply.github.com>
2024-05-30 12:07:28 +0300

    Update and rename numops.adb to nummod.adb

--- Numerics_Mod/src/nummod.adb
+++ Numerics_Mod/src/nummod.adb
@@ -1,12 +1,8 @@
--- *******************************************************************
--- *                                                                 *
--- * 20190705  Initial creation                                      *
--- *                                                                 *
--- *******************************************************************
+
 
 with Ada.Text_IO;
 
-procedure NumOps is
+procedure NumMod is
 
    package TIO renames Ada.Text_IO;
    package IIO is new TIO.Integer_IO (Integer);
